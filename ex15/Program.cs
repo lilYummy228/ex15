6080c020c40e245cdccd9ab7ee32902690e5bfc0
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-09-05 09:08:26 +0500

    доделано

--- ex15/Program.cs
+++ ex15/Program.cs
@@ -10,7 +10,26 @@ namespace ex15
     {
         static void Main(string[] args)
         {
+            int startNumber = 1;
+            int finishNumber = 28;
+            int minNumber = 100;
+            int maxNumber = 1000;
+            int result = 0;
 
+            Random random = new Random();
+            int number = random.Next(startNumber, finishNumber);
+            Console.WriteLine($"Число N: {number}");
+
+            for (int i = 0; i < maxNumber; i += number)
+            {
+                if (i >= minNumber)
+                {
+                    Console.WriteLine($"Кратное число: {i}");
+                    result++;
+                }
+            }
+
+            Console.WriteLine($"Кол-во натуральных трехзначных чисел: {result}");
         }
     }
 }
